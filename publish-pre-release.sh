2f9c5f49828a5dc8e1dd233f65d6c0a2adeeac81
Paul Wintz <19618780+pwintz@users.noreply.github.com>
2026-03-06 17:09:27 -0800

    Update publish-pre-release.sh script to check for dirty repo.

--- publish-pre-release.sh
+++ publish-pre-release.sh
@@ -3,13 +3,21 @@
 # Configure script behavior (https://stackoverflow.com/a/2871034/6651650).
 # -e script exits on error 
 # -u errors on undefined variables 
-# -x prints commands before execution 
 # -o pipefail exits on command pipe failures. 
-set -euxo pipefail
+set -euo pipefail
+
+# # Debugging
+# set -x
 
 
 # Make sure we are on the dev branch.
 git checkout dev
+
+# Check that Git repository is clean. ("-n" checks that the string is not empty.)
+if [[ -n "$(git status --porcelain)" ]]; then
+  echo "Error: The Git repository is dirty. Please commit or stash any changes."
+  exit 1
+fi
 
 # Make sure we have installed all of the npm packages
 npm install
